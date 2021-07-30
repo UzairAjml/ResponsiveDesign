ba6f4f452f97e755b57eb79fe31fe7011d3ad5cb
Uzair Ajmal <bcsf17a531@pucit.edu.pk>
2021-07-30 16:58:45 +0500

    bootstrap done

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// { } = [ ]
+"use strict";
+
+let promise = new Promise(function (resolve, reject) {
+  resolve(2);
+});
+
+promise
+  .then(first)
+  .then(second)
+  .then(function (response) {
+    console.log(response);
+  });
+
+function first(value) {
+  return value * 2;
+}
+function second(value) {
+  return value * 2;
+}
